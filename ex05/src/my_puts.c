32e92578b5885c92629db26208abb0990cee5f7d
Khlopiachyi Pavlo <khlpavlo@gmail.com>
2019-08-05 18:12:38 +0300

    ex05 done

---
+++ ex05/src/my_puts.c
@@ -0,0 +1,18 @@
+#include "../include/test.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+int my_puts(const char *s){
+    int i = 0;
+    while(s[i])
+    {
+        if(putchar(s[i]) == EOF){
+            return EOF;
+        }
+        i++;
+    }
+    if(putchar('\n') == EOF){
+        return EOF;
+    }
+    return (1);
+}
